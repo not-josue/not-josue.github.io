06729246e0366f3f13c3b0a18b8fd175298675de
not-josue <129870578+not-josue@users.noreply.github.com>
2025-02-11 09:17:20 -0500

    project part4

--- README.md
+++ README.md
@@ -6,4 +6,4 @@ This is a collection of my assignments and projects for CSCE 242 - Web Applicati
 
 🌐 [Home](https://not-josue.github.io/csce242/)
 
-📁 [Repo](https://github.com/not-josue/not-josue.github.io/tree/main/csce242)
+📁 [Folder](https://github.com/not-josue/not-josue.github.io/tree/main/csce242)
---
+++ csce242/projects/part4/js/scripts.js
@@ -0,0 +1,37 @@
+// Hamburger
+const burger = document.getElementById("hamburger");
+const mobileNav = document.getElementById("mobile-nav");
+const body = document.querySelector("body");
+
+/* 
+- Changes aria-expanded of mobile nav button
+- Toggles Mobile Menu
+- Toggles overflow to the body element to keep user from scrolling
+*/
+burger.onclick = () => {
+  const isExpanded = burger.ariaExpanded === "true";
+  burger.ariaExpanded = !isExpanded;
+  mobileNav.classList.toggle("hidden");
+  body.classList.toggle("overflow");
+};
+
+// Dark/Light Mode
+const html = document.documentElement;
+const lgThemeBtn = document.getElementById("lg-theme-btn");
+const smThemeBtn = document.getElementById("sm-theme-btn");
+
+// Toggles Light Theme and Dark Theme
+const SetTheme = (el) => {
+  el.onclick = () => {
+    html.classList.toggle("dark");
+    //   Set preferences in local storage
+    if (html.classList.contains("dark")) {
+      localStorage.theme = "dark";
+    } else {
+      localStorage.theme = "light";
+    }
+  };
+};
+
+SetTheme(lgThemeBtn);
+SetTheme(smThemeBtn);
