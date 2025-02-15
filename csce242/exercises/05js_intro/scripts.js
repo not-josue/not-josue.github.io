60305987471e367c273876300e30e1072d63ccdf
not-josue <129870578+not-josue@users.noreply.github.com>
2025-02-15 02:52:43 -0500

    project part4

--- csce242/exercises/05js_intro/scripts.js
+++ csce242/exercises/05js_intro/scripts.js
@@ -37,3 +37,74 @@ inputEl.onkeyup = (e) => {
   document.getElementById("copy").textContent =
     "You are feeling " + e.currentTarget.value;
 };
+
+// User Input
+document.getElementById("btn-choose-color").onclick = () => {
+  const inputEl = document.getElementById("txt-color");
+
+  if (!inputEl.value) return;
+
+  const pEl = document.getElementById("color-message");
+  pEl.textContent = `You choose ${inputEl.value}`;
+  pEl.style.color = inputEl.value;
+};
+
+// Ball Animation
+const AnimateBall = () => {
+  const upBtn = document.getElementById("btn-up");
+  const downBtn = document.getElementById("btn-down");
+  const inputEl = document.querySelector("#ball-area input");
+  const circle = document.getElementById("circle");
+
+  inputEl.onchange = () => {
+    // circle.style.backgroundColor = inputEl.value;
+    circle.style.setProperty("--circle-color", inputEl.value);
+  };
+
+  let pos = 0;
+
+  const changeCirclePos = (increment) => {
+    pos += increment;
+    circle.style.setProperty("top", pos + "px");
+  };
+
+  upBtn.onclick = () => {
+    changeCirclePos(-10);
+  };
+
+  downBtn.onclick = () => {
+    changeCirclePos(10);
+  };
+};
+
+AnimateBall();
+
+// Donations
+const GOAL = 1000;
+document.querySelector("#donations h2").textContent = `Goal $${GOAL}`;
+
+const Donation = () => {
+  let currentDonations = 0;
+  const buttonEl = document.getElementById("btn-add-donation");
+  const inputEl = document.getElementById("txt-donation");
+  const spanEl = document.getElementById("donation-error");
+  const pEl = document.getElementById("donation-message");
+  const divEl = document.getElementById("thermometer");
+
+  buttonEl.onclick = () => {
+    const inputValue = Number(inputEl.value);
+
+    if (isNaN(inputValue) || inputValue <= 0) {
+      spanEl.classList.remove("hidden");
+      return;
+    }
+
+    currentDonations += inputValue;
+    const donationPercent = (GOAL / 100) * 100;
+    spanEl.classList.add("hidden");
+    pEl.textContent = `${GOAL - currentDonations} To Go!`;
+    divEl.style.setProperty("--donation-percent", donationPercent);
+  };
+};
+
+Donation();
---
+++ csce242/projects/part4/promos/4/script.js
@@ -0,0 +1,62 @@
+const GetItem = async (id) => {
+  try {
+    const response = await fetch("../../data/items.json");
+    const data = await response.json();
+
+    return data.find((el) => el.id === id);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const RenderTrainer = async (num) => {
+  try {
+    const item = await GetItem(num);
+
+    // Destructure Object
+    const { action, artist, description, id, image, name, set, type } = item;
+
+    // Set Image
+    const imageEl = document.querySelector("main img");
+    imageEl.src = `../${image}`;
+    imageEl.alt = `promo card #${id} from Pokémon TCG Pocket`;
+
+    // Set Name
+    document.getElementById("card-name").textContent = name;
+
+    // // Set Type
+    document.getElementById("card-stage").textContent = type;
+
+    // Set Action
+    const actionSection = document.querySelector(".item-action");
+    const itemAction = document.createElement("p");
+    itemAction.textContent = action;
+    actionSection.append(itemAction);
+
+    // Set Description
+    const descriptionSection = document.querySelector(".item-description");
+    const itemDescription = document.createElement("p");
+    itemDescription.textContent = description;
+    descriptionSection.append(itemDescription);
+
+    // Set Collection #
+    document.getElementById("collection-number").textContent = id;
+
+    // Card Set
+    document.getElementById("card-set").textContent = set;
+
+    // Set Illustrator
+    document.getElementById("illustrator").textContent = artist;
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+const id = 4;
+RenderTrainer(id);
+
+// Description for Each Page
+const meta = document.createElement("meta");
+meta.name = "description";
+meta.content = `View the promo card #${id} from Pokémon TCG Pocket.`;
+document.head.append(meta);
