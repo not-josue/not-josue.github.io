6499bdc76a667c2e78a670ba47cef0adcdf2f524
not-josue <129870578+not-josue@users.noreply.github.com>
2025-02-20 19:31:48 -0500

    assignment07

--- csce242/assignments/assignment07/scripts.js
+++ csce242/assignments/assignment07/scripts.js
@@ -1,6 +1,48 @@
 const TREADS = 10;
+const CLIMB_RATE = 32;
 
-for (let i = 0; i < TREADS; ++i) {
-  const hr = document.createElement("hr");
-  document.getElementById("stairs").append(hr);
-}
+const img = document.querySelector("img");
+const climb = document.getElementById("climb");
+
+// Creates Stairs
+const DrawStairs = () => {
+  const stairs = document.getElementById("stairs");
+  stairs.classList.add("border-inline");
+  for (let i = 0; i < TREADS; ++i) {
+    const hr = document.createElement("hr");
+    stairs.append(hr);
+  }
+};
+
+const draw = document.getElementById("draw");
+
+draw.onclick = () => {
+  draw.setAttribute("disabled", true);
+  DrawStairs();
+  img.classList.remove("is-hidden");
+  climb.classList.remove("is-hidden");
+};
+
+// Makes img "Climb" the stairs
+
+let counter = 0;
+let stepsClimbed = 0;
+let intervalId;
+const array = ["./assets/right.png", "./assets/left.png"];
+
+const Climb = () => {
+  img.setAttribute("src", array[counter]);
+  const bottom = Number.parseInt(getComputedStyle(img).bottom);
+  img.style.bottom = `${bottom + CLIMB_RATE}px`;
+
+  ++counter;
+  if (counter === 2) counter = 0;
+
+  ++stepsClimbed;
+  if (stepsClimbed === TREADS + 1) clearInterval(intervalId);
+};
+
+climb.onclick = () => {
+  climb.setAttribute("disabled", true);
+  intervalId = setInterval(Climb, 1000);
+};
